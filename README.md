63758eeaa14ea8185c8cb94cf1f501db4a8bc4d2
Dan Stewart <dstewart@localhost.localdomain>
2015-11-02 20:16:00 +0000

    Update README and main script
    - Add Linux support.
    - Update README to document current and future functionality.
    - Fix issue with some imgur links being downloaded as wrong filetype, though some are now downloaded with a ".None" extension.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # WallpaperChanger
 
-Wallpaper changer that will randomly set a new wallpaper from /r/minimalwallpaper every 30 minutes.
-Still a work in progress - Could change design a lot depending on how effective it is.
+Wallpaper changer that will randomly set a new wallpaper from a selected subreddit every x minutes.
+
+## Current Functionality
+- Download imgur links, imgur albums and deviantart posts from subreddit.
+- Works on Linux and should work on Windows but has not been tested yet.
+
+## Future Functionality (Ordered by priority)
+- Choose subreddit to search. Current is /r/minimalwallpaper.
+- Choose number of images to fetch. Current is 25 (sorted by hot).
+- Actually change the wallpaper and set change frequency.
+- Support for Mac OS and Cygwin under Windows.
+- Add sort mode for subreddits (ie. Top, New).
+- Add functionality for downloading from multiple subreddits.
--- wallpaper.py
+++ wallpaper.py
@@ -1,18 +1,29 @@
-import ctypes, praw, urllib.request, zipfile, os, getpass
+import ctypes, praw, urllib.request, zipfile, os, getpass, imghdr
 from PIL import Image
+from sys import platform
 
-# ToDo: > Add settings - res, subreddit, limit, frequency
-# ToDo: > Change the wallpaper to a random image
+# ToDo: Add argparse to set no. of images, which subreddit and which resolution.
+#   Set wallpaper to change for each platform and each DE.
 
-username = getpass.getuser()
-imagepath = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\'
+if platform == 'linux':
+    username = getpass.getuser()
+    imagedir = '/home/' + username + '/Downloads/'
+    tmpdir = '/home/' + username + '/Downloads/tmp/'
+elif platform == 'win32':
+    username = getpass.getuser()
+    imagedir = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\'
+    tmpdir = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\tmp\\'
 
-if not os.path.exists(imagepath):
-        os.makedirs(imagepath)
+
+# If paths do not exist - make them.
+if not os.path.exists(imagedir):
+        os.makedirs(imagedir)
+if not os.path.exists(tmpdir):
+        os.makedirs(tmpdir)
 
 
 def get_image_size(image_name):
-    file = open(imagepath + image_name, 'rb')
+    file = open(imagedir + image_name, 'rb')
     img = Image.open(file)
     img.load()
     size = img.size
@@ -20,25 +31,22 @@ def get_image_size(image_name):
 
     return size
 
-
 def download_album(link):
     zip_name = "images.zip"
 
-    urllib.request.urlretrieve(link, imagepath + zip_name)  # Download the album as a zip file
+    urllib.request.urlretrieve(link, imagedir + zip_name)  # Download the album as a zip file
 
-    zip_ref = zipfile.ZipFile(imagepath + zip_name, 'r')  # Extract the zip file
-    zip_ref.extractall(imagepath)
+    zip_ref = zipfile.ZipFile(imagedir + zip_name, 'r')  # Extract the zip file
+    zip_ref.extractall(imagedir)
     zip_ref.close()
 
-    os.remove(imagepath + zip_name)  # Delete the remaining zip file
+    os.remove(imagedir + zip_name)  # Delete the remaining zip file
 
-
-def download_image(link):
+def download_image(link, path=imagedir):
     image_name = link.rpartition('/')[2]
-    urllib.request.urlretrieve(link, imagepath + image_name)
+    urllib.request.urlretrieve(link, path + image_name)
 
-    print(get_image_size(image_name))
-
+    # print(get_image_size(image_name))
 
 # Gets page source - Line 347 has the download link - seems to always be at this line no matter the image
 def download_deviantart(link):
@@ -54,7 +62,7 @@ def download_deviantart(link):
 
     image_link = link_line.split()[3].partition('"')[-1].rpartition('"')[0]
     image_name = image_link.rpartition('/')[-1]
-    urllib.request.urlretrieve(image_link, imagepath + image_name)
+    urllib.request.urlretrieve(image_link, imagedir + image_name)
 
     print(get_image_size(image_name))
 
@@ -65,23 +73,23 @@ subreddit = 'minimalwallpaper'
 r = praw.Reddit(user_agent=user_agent)
 submissions = r.get_subreddit(subreddit).get_hot(limit=25)
 
-
 for submission in submissions:
-
     if "imgur.com/a" in submission.url:
         download_album(submission.url + "/zip")  # Can append /zip to url to download zip of album
-
     elif "i.imgur.com" in submission.url:
         download_image(submission.url)
-
     elif "imgur.com" in submission.url:
-        download_image(submission.url + ".png")  # Add extension to url
-
+        download_image(submission.url + ".png", tmpdir)  # Download separately and rename to correct extension later.
     elif "deviantart.com" in submission.url:
         download_deviantart(submission.url)
-
     else:
         print("Other: " + submission.url)  # Other image hosts and self-posts
+
+
+# Fix files with wrong extensions. Some files are detected as 'None' type - so far only jpegs.
+for file in os.listdir(tmpdir):
+    ext = imghdr.what(tmpdir + file)
+    os.rename(tmpdir + file, imagedir + file.split('.')[0] + '.' + str(ext))
 
 
 # Set the wallpaper
