cae4b9e1670362722b37aa2d4ed42eb8aeba18d7
Dstewart1997 <danielandrewstewart@gmail.com>
2015-11-28 14:26:52 +0000

    Add imgur album downloader
    Can now download imgur albums, also improved clean-up to remove tmp dir as well.

--- wallpaper.py
+++ wallpaper.py
@@ -1,58 +1,93 @@
 #!/usr/bin/env python3
 
-"""Wallpaper switcher using subreddits.
-Source code: https://github.com/DStewart1997/wallpaper-changer"""
+"""
+Wallpaper switcher using subreddits.
+
+Source code: https://github.com/DStewart1997/wallpaper-changer
+"""
 
 import getpass
 import os
 from sys import platform
+import urllib.request
 import praw
+import zipfile
+
 
 class FileMgmt(object):
-    def set_paths():
+    """Sets file paths and creates non-existant paths."""
+
+    def __init__(self):
+        """Sets the paths depending on operating system and creates them if
+        they don't exist."""
+        username = getpass.getuser()
+
         if platform == 'linux':
-            username = getpass.getuser()
-            imagedir = '/home/' + username + '/Downloads/'
-            tmpdir = '/home/' + username + '/Downloads/tmp/'
+            self.imagedir = '/home/' + username + '/Pictures/wp_switcher/'
+            self.tmpdir = '/home/' + username + '/Downloads/tmp/'
         elif platform == 'win32':
-            username = getpass.getuser()
-            imagedir = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\'
-            tmpdir = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\tmp\\'
+            self.imagedir = 'C:\\Users\\' + username + \
+                '\\Pictures\\wp_switcher\\'
+            self.tmpdir = 'C:\\Users\\' + username + '\\Downloads\\tmp\\'
 
         # If paths do not exist - make them.
-        if not os.path.exists(imagedir):
-            os.makedirs(imagedir)
-        if not os.path.exists(tmpdir):
-            os.makedirs(tmpdir)
+        if not os.path.exists(self.imagedir):
+            os.makedirs(self.imagedir)
+        if not os.path.exists(self.tmpdir):
+            os.makedirs(self.tmpdir)
 
 
 class Downloader(object):
+    """Parses various links to download images from them.
+    Currently supported is imgur (and albums) and DeviantArt."""
+
     def __init__(self, link):
         self.link = link
 
     def download_imgur_album(self):
-        print("Album:", self.link)
+        """Parses imgur album links.
+        Downloads the album as a zip, extracts it, then cleans up."""
+
+        zip_name = "wp_images.zip"
+        urllib.request.urlretrieve(self.link + "/zip", fm.imagedir + zip_name)
+
+        zip_ref = zipfile.ZipFile(fm.imagedir + zip_name, 'r')
+        zip_ref.extractall(fm.imagedir)
+        zip_ref.close()
+
+        os.remove(fm.imagedir + zip_name)
+        os.rmdir(fm.tmpdir)
+
+        print("Imgur album:", self.link)
 
     def download_imgur(self):
+        """Donwloads imgur links, detects links that don't have file
+        extensions and fixes them."""
+
         print("Regular imgur:", self.link)
 
     def download_deviantart(self):
+        """Downloads images from DeviantArt links.
+        Gets the source code of the web page and parses line 347 to get direct
+        image link - seems to be reliable but a better method should be
+        used down the line."""
+
         print("Deviant Art:", self.link)
 
-
+# Sets PRAW info.
 user_agent = "Wallpaper switcher"
 subreddit = 'minimalwallpaper'
 r = praw.Reddit(user_agent=user_agent)
-submissions = r.get_subreddit(subreddit).get_hot(limit=25)
+submissions = r.get_subreddit(subreddit).get_hot(limit=50)
 
-FileMgmt.set_paths()
+fm = FileMgmt()
 
 images = []
 for submission in submissions:
     images.append(Downloader(submission.url))
 
 for img in images:
-    if "imgur/a" in img.link:
+    if "imgur.com/a" in img.link:
         Downloader.download_imgur_album(img)
     elif "imgur.com" in img.link:
         Downloader.download_imgur(img)
