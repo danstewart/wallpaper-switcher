ad650c93da10bc37b7461dca79438c0780b0639c
Dstewart1997 <danielandrewstewart@gmail.com>
2015-11-28 00:02:54 +0000

    Add skeleton functions
    Add skeleton functions for downloading from different websites and also moved file management into it's own class.

--- wallpaper.py
+++ wallpaper.py
@@ -1,31 +1,43 @@
-import ctypes, praw, urllib.request, zipfile, os, getpass, imghdr
-from PIL import Image
+#!/usr/bin/env python3
+
+"""Wallpaper switcher using subreddits.
+Source code: https://github.com/DStewart1997/wallpaper-changer"""
+
+import getpass
+import os
 from sys import platform
+import praw
 
-#  Set wallpaper to change for each platform and each DE.
-if platform == 'linux':
-    username = getpass.getuser()
-    imagedir = '/home/' + username + '/Downloads/'
-    tmpdir = '/home/' + username + '/Downloads/tmp/'
-elif platform == 'win32':
-    username = getpass.getuser()
-    imagedir = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\'
-    tmpdir = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\tmp\\'
+class FileMgmt(object):
+    def set_paths():
+        if platform == 'linux':
+            username = getpass.getuser()
+            imagedir = '/home/' + username + '/Downloads/'
+            tmpdir = '/home/' + username + '/Downloads/tmp/'
+        elif platform == 'win32':
+            username = getpass.getuser()
+            imagedir = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\'
+            tmpdir = 'C:\\Users\\' + username + '\\Pictures\\wp_switcher\\tmp\\'
+
+        # If paths do not exist - make them.
+        if not os.path.exists(imagedir):
+            os.makedirs(imagedir)
+        if not os.path.exists(tmpdir):
+            os.makedirs(tmpdir)
 
 
-# If paths do not exist - make them.
-if not os.path.exists(imagedir):
-        os.makedirs(imagedir)
-if not os.path.exists(tmpdir):
-        os.makedirs(tmpdir)
-
-        
-class Image(object):
+class Downloader(object):
     def __init__(self, link):
         self.link = link
 
     def download_imgur_album(self):
-        print()
+        print("Album:", self.link)
+
+    def download_imgur(self):
+        print("Regular imgur:", self.link)
+
+    def download_deviantart(self):
+        print("Deviant Art:", self.link)
 
 
 user_agent = "Wallpaper switcher"
@@ -33,6 +45,18 @@ subreddit = 'minimalwallpaper'
 r = praw.Reddit(user_agent=user_agent)
 submissions = r.get_subreddit(subreddit).get_hot(limit=25)
 
+FileMgmt.set_paths()
+
 images = []
 for submission in submissions:
-    images.append(Image(submission.url))
+    images.append(Downloader(submission.url))
+
+for img in images:
+    if "imgur/a" in img.link:
+        Downloader.download_imgur_album(img)
+    elif "imgur.com" in img.link:
+        Downloader.download_imgur(img)
+    elif "deviantart.com" in img.link:
+        Downloader.download_deviantart(img)
+    else:
+        print("Other:", img.link)
